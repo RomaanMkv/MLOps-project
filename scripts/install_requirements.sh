4278040feeac4d59039d717929b9df16f33d42f9
RomaanMkv <romanmakeev519@gmail.com>
2024-06-27 13:20:15 +0300

    minor update

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-# Navigate to the script's directory
-cd "$(dirname "$0")"
+# Function to navigate up to the MLOps-project directory
+navigate_to_mlops_project() {
+  current_dir=$(pwd)
+  target_dir="MLOps-project"
 
-# Navigate to the project root directory
-cd ..
+  while [ "$(basename "$current_dir")" != "$target_dir" ]; do
+    current_dir=$(dirname "$current_dir")
+    if [ "$current_dir" == "/" ]; then
+      echo "MLOps-project directory not found."
+      return
+    fi
+  done
+
+  cd "$current_dir" || return
+  echo "Changed directory to: $current_dir"
+}
+
+# Call the function
+navigate_to_mlops_project
 
 # Check if virtual environment folder exists, if not, create one
 if [ ! -d "venv" ]; then
-    python3 -m venv venv
+    python3.11 -m venv venv
 fi
 
 # Activate the virtual environment
