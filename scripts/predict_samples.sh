55757b0944a4d9a10161d45e80d9010e9c3e69cd
RomaanMkv <romanmakeev519@gmail.com>
2024-07-16 21:19:06 +0300

    feat: flask API has been created

--- scripts/predict_samples.sh
+++ scripts/predict_samples.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Function to navigate up to the MLOps-project directory
+navigate_to_mlops_project() {
+  current_dir=$(pwd)
+  target_dir="MLOps-project"
+
+  while [ "$(basename "$current_dir")" != "$target_dir" ]; do
+    current_dir=$(dirname "$current_dir")
+    if [ "$current_dir" == "/" ]; then
+      echo "MLOps-project directory not found."
+      return
+    fi
+  done
+
+  cd "$current_dir" || return
+  echo "Changed directory to: $current_dir"
+}
+
+# Call the function
+navigate_to_mlops_project
+
 # Loop to run the script 5 times with different example_version values
 for version in {1..5}
 do
