3d0d8b2725490a61ebbf0da9c24c1974708ecf40
kezouke <eliwhatthe@gmail.com>
2024-07-22 12:06:04 +0000

    test fix

--- tests/test_sample_data.py
+++ tests/test_sample_data.py
@@ -22,7 +22,6 @@ def temp_dirs():
     temp_input_dir.cleanup()
     temp_output_dir.cleanup()
 
-
 @pytest.fixture
 def sample_data_cfg(temp_dirs):
     temp_input_dir, temp_output_dir = temp_dirs
@@ -36,43 +35,41 @@ def sample_data_cfg(temp_dirs):
     df.to_csv(mock_data_path, index=False)
 
     sample_data_cfg_dict = {
-        "data": {
+        "sample_data": {
             "path": temp_input_dir.name + "/",
             "raw_data_name": "test_data",
-            "version": "v1",
+            "sample_version": "v1",  # Add sample_version here
             "data_format": ".csv",
             # Different sample sizes to test
             "sample_sizes": [0.1, 0.5, 0.8],
-            "sample_folder": temp_input_dir.name + "/data/sample_data"
+            "sample_folder": temp_output_dir.name + "/data/sample_data"
         }
     }
 
     return OmegaConf.create(sample_data_cfg_dict)
 
-
 @pytest.mark.usefixtures("temp_dirs", "sample_data_cfg")
 def test_sample_data_handles_different_sample_sizes(sample_data_cfg):
     # Call sample_data with different sample sizes
-    for sample_size in sample_data_cfg.data.sample_sizes:
-        sample_data_cfg.data.sample_size = sample_size
+    for sample_size in sample_data_cfg.sample_data.sample_sizes:
+        sample_data_cfg.sample_data.sample_size = sample_size
         sample_data(sample_data_cfg)
 
         # Check that the sample was saved to the expected location
-        expected_sample_path = os.path.join(sample_data_cfg.data.sample_folder, f"sample.csv")
+        expected_sample_path = os.path.join(sample_data_cfg.sample_data.sample_folder, "sample.csv")
         assert os.path.exists(expected_sample_path), f"Expected sample.csv to be created but found nothing."
 
 @pytest.mark.usefixtures("temp_dirs", "sample_data_cfg")
 def test_sample_data_saves_sample(sample_data_cfg):
     # Set sample_size for the first sample size in the list
-    sample_data_cfg.data.sample_size = sample_data_cfg.data.sample_sizes[0]
+    sample_data_cfg.sample_data.sample_size = sample_data_cfg.sample_data.sample_sizes[0]
     
     # Call sample_data with the configured settings
     sample_data(sample_data_cfg)
 
     # Check that the sample was saved to the expected location
-    expected_sample_path = os.path.join(sample_data_cfg.data.sample_folder, "sample.csv")
+    expected_sample_path = os.path.join(sample_data_cfg.sample_data.sample_folder, "sample.csv")
     assert os.path.exists(expected_sample_path), f"Expected sample.csv to be created but found nothing."
-
 
 if __name__ == "__main__":
     pytest.main()
